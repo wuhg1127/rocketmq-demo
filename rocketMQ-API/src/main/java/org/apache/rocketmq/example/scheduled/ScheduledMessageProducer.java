10133cfc906fd7ba59e048c6ed7b98f47ea1a8f8
wuhaigang <wuhg1127@163.com>
2021-03-08 23:20:26 +0800

    rocketMQ for example

---
+++ rocketMQ-API/src/main/java/org/apache/rocketmq/example/scheduled/ScheduledMessageProducer.java
@@ -0,0 +1,27 @@
+package org.apache.rocketmq.example.scheduled;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.common.message.Message;
+
+public class ScheduledMessageProducer {
+    
+     public static void main(String[] args) throws Exception {
+         // Instantiate a producer to send scheduled messages
+         DefaultMQProducer producer = new DefaultMQProducer("ExampleProducerGroup");
+         producer.setNamesrvAddr("192.168.0.66:9876;192.168.0.67:9876;192.168.0.68:9876");
+         // Launch producer
+         producer.start();
+         int totalMessagesToSend = 100;
+         for (int i = 0; i < totalMessagesToSend; i++) {
+             Message message = new Message("TopicTest", ("Hello scheduled message " + i).getBytes());
+             // This message will be delivered to consumer 10 seconds later.
+             message.setDelayTimeLevel(3);
+             // Send the message
+             producer.send(message);
+         }
+    
+         // Shutdown producer after use.
+         producer.shutdown();
+     }
+        
+ }
